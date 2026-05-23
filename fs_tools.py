8006f6a7305cc894e0eb2a31cb7afc79e2d1f5b8
Sarkar <sarkarrohit650@gmail.com>
2026-05-24 02:30:21 +0530

    phase 3: read files for pdf added

--- fs_tools.py
+++ fs_tools.py
@@ -1,10 +1,11 @@
-import os
+import os,fitz
 
 def read_file(filepath):
     if not os.path.exists(filepath):
         return {"success": False, "error": "File not found"}
     
     if filepath.endswith(".txt"):
+        content = ""
         with open(filepath, "r", encoding="utf-8") as f:
             content = f.read()
         return {
@@ -13,12 +14,23 @@ def read_file(filepath):
             "extension": ".txt",
             "content": content
         }
+    elif filepath.endswith(".pdf"):
+        content = ""
+        with fitz.open(filepath) as pdf_file:
+            for page in pdf_file:
+                content += page.get_text()
 
+        return {
+            "success": True,
+            "filepath": filepath,
+            "extension": ".pdf",
+            "content": content
+        }
     
     else:
         return {"success": False, "error": "File type not supported yet"}
     
 
 if __name__ == "__main__":
-    result = read_file("resumes/test_1.txt")
+    result = read_file("resumes/test_2.pdf")
     print(result)
