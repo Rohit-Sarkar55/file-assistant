06e3cb8757152ca85fe0ecfe87bab0d9605b7b9f
Sarkar <sarkarrohit650@gmail.com>
2026-05-24 02:57:04 +0530

    phase 5: list file feature added

--- fs_tools.py
+++ fs_tools.py
@@ -1,4 +1,6 @@
 import os,fitz
+from docx import Document
+from datetime import datetime
 
 def read_file(filepath):
     if not os.path.exists(filepath):
@@ -27,10 +29,62 @@ def read_file(filepath):
             "content": content
         }
     
+    elif filepath.endswith(".docx"):
+        content = ""
+        doc = Document(filepath)
+        for paragraph in doc.paragraphs:
+            if paragraph.text.strip():
+                content += paragraph.text + "\n"
+
+        return {
+            "success": True,
+            "filepath": filepath,
+            "extension": ".docx",
+            "content": content
+        }
+    
     else:
         return {"success": False, "error": "File type not supported yet"}
     
 
+def list_files(directory, extension=None):
+
+    if not os.path.exists(directory):
+        return {"success": False, "error": "Directory not found"}
+
+    files = []
+
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+
+        # Skip if it is not a file
+        if not os.path.isfile(filepath):
+            # print(f"Skipping non-file: {filename}")
+            continue
+
+        # Filter by extension if provided
+        if extension and not filename.endswith(extension):
+            continue
+
+        # Get file metadata
+        size = os.path.getsize(filepath)
+        modified = os.path.getmtime(filepath)
+        modified_readable = datetime.fromtimestamp(modified).strftime("%Y-%m-%d %H:%M:%S")
+
+        files.append({
+            "name": filename,
+            "filepath": filepath,
+            "size_bytes": size,
+            "modified": modified_readable
+        })
+
+    return {"success": True, "directory": directory, "files": files}
+    
+
 if __name__ == "__main__":
-    result = read_file("resumes/test_2.pdf")
+    # result = read_file("resumes/test_3.docx")
+    result = list_files("resumes")
+    print(result)
+
+    result = list_files("resumes", ".pdf")
     print(result)
